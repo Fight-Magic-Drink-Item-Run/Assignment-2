72c958a96f3f6cce53e7fbd84cead42e59853039
Fight-Magic-Drink-Item-Run <46539950+Fight-Magic-Drink-Item-Run@users.noreply.github.com>
2019-01-19 21:58:11 -0700

    Update Assignment 2 py.py
    Seond array with k= 15 variables

--- Assignment 2 py.py
+++ Assignment 2 py.py
@@ -82,3 +82,44 @@ selectionSort4
 selectionSort5
 
 selectionSort = np.array([selectionSort1, selectionSort2, selectionSort3, selectionSort4])
+
+k15array_string5000 = [''.join(random.choices(string.ascii_letters, k = 15)) for _ in range(5000)]
+k15array_string10000 = [''.join(random.choices(string.ascii_letters, k = 15)) for _ in range(10000)]
+k15array_string15000 = [''.join(random.choices(string.ascii_letters, k = 15)) for _ in range(15000)]
+k15array_string20000 = [''.join(random.choices(string.ascii_letters, k = 15)) for _ in range(20000)]
+k15array_string25000 = [''.join(random.choices(string.ascii_letters, k = 15)) for _ in range(25000)]
+
+
+start_time = time.time()
+selectionSort(k15array_string5000)
+end_time = time.time()
+k15selectionSort1 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(k15array_string10000)
+end_time = time.time()
+k15selectionSort2 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(k15array_string15000)
+end_time = time.time()
+k15selectionSort3 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(k15array_string20000)
+end_time = time.time()
+k15selectionSort4 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(k15array_string25000)
+end_time = time.time()
+k15selectionSort5 = (end_time - start_time)
+
+
+k15selectionSort1
+k15selectionSort2
+k15selectionSort3
+k15selectionSort4
+k15selectionSort5
+
+k15selectionSort = np.array([k15selectionSort1, k15selectionSort2, k15selectionSort3, k15selectionSort4])
