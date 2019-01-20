f958868b5b07eae668648ca9dffb80712bd21504
Fight-Magic-Drink-Item-Run <46539950+Fight-Magic-Drink-Item-Run@users.noreply.github.com>
2019-01-19 22:01:02 -0700

    Update Assignment 2 py.py
    Final float array and plots needed for assignment

--- Assignment 2 py.py
+++ Assignment 2 py.py
@@ -162,3 +162,88 @@ IntselectionSort4
 IntselectionSort5
 
 IntselectionSort = np.array([IntselectionSort1, IntselectionSort2, IntselectionSort3, IntselectionSort4])
+
+array_float5000 = list(np.random.random(5000))
+array_float10000 = list(np.random.random(10000))
+array_float15000 = list(np.random.random(15000))
+array_float20000 = list(np.random.random(20000))
+array_float25000 = list(np.random.random(25000))
+
+start_time = time.time()
+selectionSort(array_float5000)
+end_time = time.time()
+floatselectionSort1 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(array_float10000)
+end_time = time.time()
+floatselectionSort2 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(array_float15000)
+end_time = time.time()
+floatselectionSort3 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(array_float20000)
+end_time = time.time()
+floatselectionSort4 = (end_time - start_time)
+
+start_time = time.time()
+selectionSort(array_float25000)
+end_time = time.time()
+floatselectionSort5 = (end_time - start_time)
+
+floatselectionSort1
+floatselectionSort2
+floatselectionSort3
+floatselectionSort4
+floatselectionSort5
+
+floatselectionSort = np.array([floatselectionSort1, floatselectionSort2, floatselectionSort3, floatselectionSort4])
+
+import prettytable
+from prettytable import PrettyTable
+t = PrettyTable(['array string 5', 'array string 15', 'array int', 'array float'])
+t.add_row([selectionSort1, k15selectionSort1, IntselectionSort1, floatselectionSort1])
+t.add_row([selectionSort2, k15selectionSort2, IntselectionSort2, floatselectionSort2])
+t.add_row([selectionSort3, k15selectionSort3, IntselectionSort3, floatselectionSort3])
+t.add_row([selectionSort4, k15selectionSort4, IntselectionSort4, floatselectionSort4])
+print(t)
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+array5lenght1 = len(array_string5000)
+array5lenght2 = len(array_string10000)
+array5lenght3 = len(array_string15000)
+array5lenght4 = len(array_string20000)
+
+array5length = np.array([array5lenght1, array5lenght2, array5lenght3, array5lenght4])
+
+array15lenght1 = len(k15array_string5000)
+array15lenght2 = len(k15array_string10000)
+array15lenght3 = len(k15array_string15000)
+array15lenght4 = len(k15array_string20000)
+
+array15length = np.array([array15lenght1, array15lenght2, array15lenght3, array15lenght4])
+
+int_lenght1 = len(array_int5000)
+int_lenght2 = len(array_int10000)
+int_lenght3 = len(array_int15000)
+int_lenght4 = len(array_int20000)
+
+int_length = np.array([int_lenght1, int_lenght2, int_lenght3, int_lenght4])
+
+float_lenght1 = len(array_float5000)
+float_lenght2 = len(array_float10000)
+float_lenght3 = len(array_float15000)
+float_lenght4 = len(array_float20000)
+
+float_lenght = np.array([float_lenght1, float_lenght2, float_lenght3, float_lenght4])
+
+plt.plot(array5length, selectionSort)
+plt.plot(array15length, k15selectionSort)
+plt.plot(int_length, IntselectionSort)
+plt.plot(float_lenght, floatselectionSort)
